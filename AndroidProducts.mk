d49267faee3da041931e9932a14b3ba91adc0afa
TheBeast0403 <89011564+TheBeast0403@users.noreply.github.com>
2021-08-17 22:03:50 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@
 # Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
 # The part of last line in mine looks like "omni_Primo_RX5.mk"
 
-LOCAL_PATH := device/HMD Global/WSP_sprout
+LOCAL_PATH := device/Nokia/WSP_sprout
 
 PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_WSP_sprout.mk
